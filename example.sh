064b49853402d53df082b168e14bde8085047a89
Niklas Frondorf <niklas.frondorf@web.de>
2023-04-06 17:41:06 +0200

    fixed error in command

--- example.sh
+++ example.sh
@@ -1,3 +1,3 @@
-for i in {1..3}; do
-    cargo run --bin main -- img --preset=$i --width=1200 --height=800 --anti-aliasing=2 --max-depth=4 --occlusion-offset=0.1 --output out --versionize
+for i in {1..4}; do
+    cargo run --bin raytracer -- img --preset=$i --width=1200 --height=800 --anti-aliasing=2 --max-depth=4 --occlusion-offset=0.1 --output out --versionize
 done;
